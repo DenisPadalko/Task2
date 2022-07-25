d668fba925fbd835265219f86a26f5a98ceda824
Padalko Denis <denispadlko@gmail.com>
2022-07-25 15:00:19 +0300

    add prototypes of constructors and destructor

--- Task2/Class.h
+++ Task2/Class.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <iostream>
+
+class Matrix 
+{
+private:
+	double** MatrixElements;
+	int* Columns, Lines;
+public:
+	Matrix();
+	Matrix(const double** AnotherMatrix, const int AnotherMatrixColumns, const int AnotherMatrixLines);
+	Matrix(const Matrix& AnotherMatrix);
+	Matrix(const Matrix&& AnotherMatrix);
+	Matrix(const int Number);
+	~Matrix();
+	Matrix& operator=(const Matrix& AnotherMatrix);
+	Matrix& operator=(const Matrix&& AnotherMatrix);
+};
